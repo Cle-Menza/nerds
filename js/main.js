038dcd601b0ac62f9816dc42bada9e54b68fdaee
Cle-Menza <mukovoz.igor@gmail.com>
2017-02-28 11:57:01 +0200

    ++

---
+++ js/main.js
@@ -0,0 +1,24 @@
+var link = document.querySelector('.map_contacts_btn');
+var popupForm = document.querySelector('.popup-form');
+var closePopup = document.querySelector('.popup-form-btn-close');
+
+link.addEventListener('click', function(event) {
+	event.preventDefault();
+	popupForm.classList.add('anim')
+	popupForm.classList.add('show-popup')
+});
+
+closePopup.addEventListener('click', function(event) {
+	event.preventDefault();
+	popupForm.classList.remove('show-popup')
+	popupForm.classList.remove('anim')
+});
+
+window.addEventListener('keydown', function(event) {
+	if (event.keyCode === 27) {
+		if (popupForm.classList.contains('show-popup')) {
+			popupForm.classList.remove('show-popup')
+			popupForm.classList.remove('anim')
+		}
+	}
+});
