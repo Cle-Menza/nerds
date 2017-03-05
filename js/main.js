5813faeabc493930a6f688a03c80219922045071
Cle-Menza <mukovoz.igor@gmail.com>
2017-03-05 13:07:47 +0200

    ++

--- js/main.js
+++ js/main.js
@@ -1,24 +1,35 @@
 var link = document.querySelector('.map_contacts_btn');
 var popupForm = document.querySelector('.popup-form');
 var closePopup = document.querySelector('.popup-form-btn-close');
+var overlay = document.querySelector('.overlay');
+var sliders = document.querySelector('.sliders');
 
-link.addEventListener('click', function(event) {
+link.addEventListener('click', function (event) {
 	event.preventDefault();
 	popupForm.classList.add('anim')
 	popupForm.classList.add('show-popup')
+	overlay.classList.add('show-popup')
 });
 
-closePopup.addEventListener('click', function(event) {
+closePopup.addEventListener('click', function (event) {
 	event.preventDefault();
 	popupForm.classList.remove('show-popup')
 	popupForm.classList.remove('anim')
+	overlay.classList.remove('show-popup')
 });
 
-window.addEventListener('keydown', function(event) {
+window.addEventListener('keydown', function (event) {
 	if (event.keyCode === 27) {
 		if (popupForm.classList.contains('show-popup')) {
 			popupForm.classList.remove('show-popup')
 			popupForm.classList.remove('anim')
+			overlay.classList.remove('show-popup')
 		}
 	}
+});
+
+overlay.addEventListener('click', function (e) {
+	overlay.classList.remove('show-popup')
+	popupForm.classList.remove('show-popup')
+	popupForm.classList.remove('anim')
 });
